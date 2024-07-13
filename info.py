b03aa378e86044ca59238b2f6c016bf306d6a809
Su6666666 <86417238+Su6666666@users.noreply.github.com>
2024-07-13 06:27:47 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,8 +1,8 @@
-API_ID       = 
-API_HASH     = ""
+API_ID       = 26042863
+API_HASH     = "d4fabc00b0345cd3f0ccdc0c9b750f6e"
 BOT_TOKEN    = ""
-SESSION      = ""
-DATABASE_URI = ""
-LOG_CHANNEL  = -1001884373226
-ADMIN        = 1350642629
-CHANNEL      = "@GreyMatter_Bots"
+SESSION      = "BQD6zg0AxJkocbrkBWYl7WF3SpqF330FqQy7g1k5dRhwx6kHiBoeqT895xp6RcJ9LdGD66F4ogBkAu_OYpdmzk5FaZiXJ562P3tK5pY_212GyfoB_DkQY_L0LCRY0I-T9y6y_Mcu0zrv4WDDWwJWudeKm-2uomA8j2f_4HUV1jmgtZLhEzW3oB3EWeyCQf9g_FnMPuZuyA8g_XzTNOCUGeod46GDx-a6EIHuXab6I3UyIxBbBjbxS8iFuOgFn_mkWT44wcbWZz_s-n7NO6dO9EELKqA03qewMoTZj0UdEuDLq34WEztcMGAmTJpC0cQEnsfynREikptndWFbfP5Xmvybf4CrUQAAAAGpxhs1AA"
+DATABASE_URI = "mongodb+srv://SGBACKUP4:SGBACKUP@cluster0.zjpcla3.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+LOG_CHANNEL  = -1001994332079
+ADMIN        = 919169586
+CHANNEL      = ""
